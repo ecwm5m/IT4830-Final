fe00dc5ccc618ee9d70fb97ecbfac0cf2c0b753b
Elijah Woods <elijahcwoods10@gmail.com>
2023-04-28 14:22:01 -0500

    Components

---
+++ finalproject/src/app/pages/daily/daily.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-daily',
+  templateUrl: './daily.component.html',
+  styleUrls: ['../../../styles.css']
+})
+
+export class DailyComponent{
+
+}
