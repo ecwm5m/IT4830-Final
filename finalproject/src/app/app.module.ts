10e5d829a7390260d96975626327b5a490621c0b
Elijah Woods <elijahcwoods10@gmail.com>
2023-04-29 09:52:34 -0500

    header/footer

--- finalproject/src/app/app.module.ts
+++ finalproject/src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DailyComponent } from './pages/daily/daily.component';
 import { WeeklyComponent } from './pages/weekly/weekly.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     DailyComponent,
-    WeeklyComponent
+    WeeklyComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
