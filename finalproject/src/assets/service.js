0ed158e7b55640a36207c93701809b4ee673ff85
nttucker28 <nttucker28@gmail.com>
2023-05-04 21:55:27 -0500

    api fixes

--- finalproject/src/assets/service.js
+++ finalproject/src/assets/service.js
@@ -1,19 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Finalproject</title>
-  <base href="/">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="favicon.ico">
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-</head>
-<body class="mat-typography">
-  <app-root></app-root>
-
-  <script>
 function getweatherweekly(){
   var city=document.getElementById("search").value;
   fetch("http://api.weatherstack.com/historical?access_key=2e67ddb608d0f7d5e5213848ead4f0b4&query="+city+"&units=f&historical_date=2023-05-03&hourly=1")
@@ -102,6 +86,3 @@ function getweather(){
 
   });
 }
-  </script>
-</body>
-</html>
